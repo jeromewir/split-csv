5d22ce0357e57c6ce29517efcf17ecb3f7284f54
jeromewir <jeromwir@gmail.com>
2017-01-24 16:48:39 +0100

    Use readline instead of liner

--- example/example.js
+++ example/example.js
@@ -3,5 +3,4 @@ const splitCsv = require('../lib/index.js');
 splitCsv.splitByLines('files/smallFile.csv', { dest: 'dest', nbLines: 100 })
   .then((files) => {
     console.log(files);
-  })
-  .catch(err => console.error(err.stack));
+  }).catch(err => console.error(err.stack));
