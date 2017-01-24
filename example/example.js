a14b5ec98c82824a386f3fd20c414f7e5d5dd9a7
jeromewir <jeromwir@gmail.com>
2017-01-24 15:38:12 +0100

    Added files

---
+++ example/example.js
@@ -0,0 +1,7 @@
+const splitCsv = require('../lib/index.js');
+
+splitCsv.splitByLines('files/smallFile.csv', { dest: 'dest', nbLines: 100 })
+  .then((files) => {
+    console.log(files);
+  })
+  .catch(err => console.error(err.stack));
